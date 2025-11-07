b9e0605b37b849a7c4f41fc982d7b203c7ec807a
Lokesh <lokesh972007@gmail.com>
2025-11-07 23:35:27 +0530

    Fix lazy loading exception on server

--- src/main/java/com/example/sdms/repository/ResultRepository.java
+++ src/main/java/com/example/sdms/repository/ResultRepository.java
@@ -1,9 +1,9 @@
 package com.example.sdms.repository;
 
-import com.example.sdms.model.Results; // Changed to singular
+import com.example.sdms.model.Results;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ResultRepository extends JpaRepository<Results, Long> { // Changed to singular
+public interface ResultRepository extends JpaRepository<Results, Long> {
 }
